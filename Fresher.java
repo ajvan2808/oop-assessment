fb3b4ffb9c5bd3ea406bcda4c13024a64ef7602f
ajvan2808 <vhh2808@gmail.com>
2022-07-19 00:26:46 +0700

    adding toString Fresher class

--- Fresher.java
+++ Fresher.java
@@ -31,7 +31,10 @@ public class Fresher extends Employee{
 	public String getEducation() {
 		return education;
 	}
-	public void setEducation(String education) {
+	public void setEducation(String education) throws Exception {
+		if (education.isBlank()) {
+			throw new Exception("Education cannot be blank");
+		}
 		this.education = education;
 	}
 
@@ -69,10 +72,31 @@ public class Fresher extends Employee{
 		setGraduationRank(sc.nextLine());
 
 		System.out.println("Enter level of education: ");
-		
-		
+		do {
+			try {
+				setEducation(sc.nextLine());
+			} catch (Exception e) {
+				System.out.println("Try again: ");
+				sc.nextLine();
+			}
+		} while (this.education == null);
+	}
 
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
 
+		sb.append("Employee: ");
+		sb.append("\nID: ").append(this.getID());
+		sb.append("\nFullname: ").append(this.getFullname());
+		sb.append("\nBirthday: ").append(this.getBirthday());
+		sb.append("\nPhone: ").append(this.getPhone());
+		sb.append("\nEmail: ").append(this.getEmail());
+		sb.append("\nEmployee type: ").append(this.getEmployee_type());
+		sb.append("\nGraduation date: ").append(graduationDate);
+		sb.append("\nGraduation rank: ").append(graduationRank);
+		sb.append("\nEducation: ").append(education);
 		
+		return sb.toString();
 	}
 }
